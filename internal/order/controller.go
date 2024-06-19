571ce9c364af28e831946a3147fd3357c0c73424
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2024-06-19 14:38:32 +0700

    remove inject on ctrl

--- internal/order/controller.go
+++ internal/order/controller.go
@@ -1,9 +1,7 @@
 package order
 
 import (
-	"fmt"
 	"hrz8/gofx/internal/core"
-	"hrz8/gofx/model"
 	"hrz8/gofx/port"
 	"net/http"
 
@@ -14,30 +12,18 @@ type controller struct {
 	logger *core.Logger
 	config *core.Config
 	svc    port.OrderService
-	uSvc   port.UserService
 }
 
-func NewController(log *core.Logger, cfg *core.Config, svc port.OrderService, uSvc port.UserService) *controller {
+func NewController(log *core.Logger, cfg *core.Config, svc port.OrderService) *controller {
 	return &controller{
 		logger: log,
 		config: cfg,
 		svc:    svc,
-		uSvc:   uSvc,
 	}
 }
 
 func (ctrl *controller) CreateOrder(c echo.Context) error {
-	var err error
-	var user *model.User
-
-	user, err = ctrl.uSvc.GetUserFromDB(false)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err)
-	}
-
-	ctrl.logger.Info(fmt.Sprintf("creating order on %s for user %s...", ctrl.config.AppName, user.Name))
-
-	if err = ctrl.svc.PersistOrderData(false); err != nil {
+	if err := ctrl.svc.PersistOrderData(false); err != nil {
 		return c.JSON(http.StatusInternalServerError, err)
 	}
 	return c.JSON(http.StatusOK, "success create order")
--- internal/order/service.go
+++ internal/order/service.go
@@ -3,23 +3,36 @@ package order
 import (
 	"fmt"
 	"hrz8/gofx/internal/core"
+	"hrz8/gofx/model"
+	"hrz8/gofx/port"
 )
 
 type service struct {
 	logger *core.Logger
 	config *core.Config
+	uSvc   port.UserService
 }
 
-func NewService(log *core.Logger, cfg *core.Config) *service {
+func NewService(log *core.Logger, cfg *core.Config, uSvc port.UserService) *service {
 	return &service{
 		logger: log,
 		config: cfg,
+		uSvc:   uSvc,
 	}
 }
 
-func (s *service) PersistOrderData(err bool) error {
+func (s *service) PersistOrderData(shouldErr bool) error {
+	var user *model.User
+
+	user, err := s.uSvc.GetUserFromDB(false)
+	if err != nil {
+		return err
+	}
+
+	s.logger.Info(fmt.Sprintf("creating order on %s for user %s...", s.config.AppName, user.Name))
+
 	s.logger.Info("persisting order data into database")
-	if err {
+	if shouldErr {
 		return fmt.Errorf("persist order data error")
 	}
 	return nil
